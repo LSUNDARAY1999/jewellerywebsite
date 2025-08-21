cb5c650b8a3be4822c534b31db9766e29b356c9c
LuckySundaray <161884502+LuckySundaray@users.noreply.github.com>
2025-08-22 00:54:44 +0530

    first copy

--- src/components/header/Card.jsx
+++ src/components/header/Card.jsx
@@ -3,11 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 // import './Card.css'; // For animation
 
-
 function Card() {
   return (
     <div className="container my-5">
-      <div className="row">
+      <div className="row" style={{justifyContent:"space-around"}}>
         {/* Card 1 */}
         <div className="col-md-4">
           <div className="custom-card">
@@ -27,7 +26,7 @@ function Card() {
         <div className="col-md-4">
           <div className="custom-card">
             <div className="custom-card-img">
-              <img src="src/assets/card/card2.jpg" alt="Card 2" />
+              <img src="src/assets/slider/slider2.jpg" alt="Card 2" />
             </div>
             <div className="custom-card-body" >
               <h5>Card Title 2</h5>
@@ -38,64 +37,44 @@ function Card() {
           </div>
         </div>
 
-        {/* Carousel */}
 
-    <div className="col-md-4" id="card-slide">
-      <div
-        id="carouselExampleFade"
-        className="carousel slide carousel-fade"
-        data-bs-ride="carousel"
-      >
-        <div
-          className="carousel-inner"
-          style={{ height: '400px', width: '400px' }}
-        >
-          <div className="carousel-item active">
-            <img
-              src="src/assets/slider/slider1.jpg"
-              className="d-block w-100"
-              alt="Slide 1"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="src/assets/slider/slider2.jpg"
-              className="d-block w-100"
-              alt="Slide 2"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="src/assets/slider/slider3.jpg"
-              className="d-block w-100"
-              alt="Slide 3"
-            />
+               <div className="col-md-4">
+          <div className="custom-card">
+            <div className="custom-card-img">
+              <img src="src/assets/card/card2.jpg" alt="Card 2" />
+            </div>
+            <div className="custom-card-body" >
+              <h5>Card Title 2</h5>
+              <p>
+                Some quick example text to build on the card title and make up the bulk of the card’s content.
+              </p>
+            </div>
           </div>
         </div>
-
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div>
-    </div>
-    </div>
  
+        {/* Carousel */}
+
+            {/* <div className="carousel-item">
+          <img src="src/assets/banners/banner4.jpg" className="d-block w-100" alt="Slide 2" />
+        </div>
+        <div className="carousel-item">
+          <img src="src/assets/banners/banner5.jpg" className="d-block w-100" alt="Slide 3" />
+        </div>
+        <div className="carousel-item">
+          <img src="src/assets/banners/banner6.jpg" className="d-block w-100" alt="Slide 4"  />
+        </div>
+      </div>
+
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button> */}
+    </div>  
+    </div>
   );
 };
 
